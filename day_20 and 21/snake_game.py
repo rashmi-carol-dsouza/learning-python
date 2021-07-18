53c93ae06be56ae9df88ffebf12b0deb97b3c41a
Rashmi Carol Dsouza <hello@rashmidsouza.com>
2021-07-18 11:23:09 +0530

    updates

--- day_20 and 21/snake_game.py
+++ day_20 and 21/snake_game.py
@@ -34,15 +34,21 @@ while game_is_on:
 
     #Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        # game_is_on = False
+        # scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     #Detect collision with tail.
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            # game_is_on = False
+            # scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+
+
 
 screen.exitonclick()
