c0df742e128015ad4b047ac322bcfc306884fd1a
Rashmi Carol Dsouza <hello@rashmidsouza.com>
2021-07-19 21:12:55 +0530

    updates

--- day_20 and 21/scoreboard.py
+++ day_20 and 21/scoreboard.py
@@ -7,7 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open("data.txt") as data:
+        with open("c:/Users/yser/Desktop/learining_python/day_20 and 21/data.txt") as data:
             self.high_score = int(data.read())    
         self.color("white")
         self.penup()
@@ -22,7 +22,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-            with open("data.txt", mode="w") as data:
+            with open("c:/Users/yser/Desktop/learining_python/day_20 and 21/data.txt", mode="w") as data:
                 data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
