bd42d36b391f64c0778facf63580b061404b6274
Rashmi Carol Dsouza <hello@rashmidsouza.com>
2021-07-19 17:51:56 +0530

    updates

--- day_20 and 21/scoreboard.py
+++ day_20 and 21/scoreboard.py
@@ -2,13 +2,13 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 18, "normal")
 
-
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())    
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,6 +22,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
