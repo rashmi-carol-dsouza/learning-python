7291f9e87fac8c5df98acecb69cb83638211b750
Rashmi Carol Dsouza <hello@rashmidsouza.com>
2021-07-24 23:35:56 +0530

    new learnings

--- day_25/US States Game/main.py
+++ day_25/US States Game/main.py
@@ -19,8 +19,7 @@ all_states = data.state.to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
-                                    prompt="What's another state's name?").title()
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",prompt="What's another state's name?").title()
     if answer_state == "Exit":
         missing_states = []
         for state in all_states:
